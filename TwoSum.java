312c4b70aab7d24b630e163732c9f17c6ca7fe68
Whilmar Bitoco <whlmrbitoco@gmail.com>
2024-10-17 14:13:09 +0800

    solved two-sum problem

---
+++ TwoSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] list, int target) {
+        for (int j = 0; j < list.length - 1; j++) {
+            for (int k = list.length - 1; k >= 0; k--) {
+                int value = list[j] + list[k];
+
+                if (value == target && j != k) {
+                    return new int[] { k, j };
+                }
+
+            }
+
+        }
+
+        return null;
+    }
+}
