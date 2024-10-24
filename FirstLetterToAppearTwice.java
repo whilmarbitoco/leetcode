a70894d9a495915443e62522569411c23e03261b
Whilmar Bitoco <whlmrbitoco@gmail.com>
2024-10-24 19:32:14 +0800

    solved first-letter-to-appear-twice/submissions problem

---
+++ FirstLetterToAppearTwice.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public char repeatedCharacter(String s) {
+
+        Set<Character> chars = new HashSet<>();
+
+        return '0';
+    }
+}
